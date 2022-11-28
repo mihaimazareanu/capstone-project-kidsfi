a1a745d2ee9a7f13856f256d3f9a1f9d3a64c3cf
Mihai Mazareanu <mihai.mazareanu@gmail.com>
2022-11-28 16:44:08 +0100

    moved GlobalStyles outside of the Layout component, so that everything will get these styles, not only the Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,12 @@ import Layout from "../components/Layout";
 
 function MyApp({Component, pageProps}) {
   return (
-    <Layout>
+    <>
       <GlobalStyles />
-      <Component {...pageProps} />
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
