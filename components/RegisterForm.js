c03e692e2b8de2320672f3e01a3332a2df28c843
Mihai Mazareanu <mihai.mazareanu@gmail.com>
2022-12-03 17:04:58 +0100

    minor lint change, so that the code is accepted

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -10,7 +10,6 @@ export default function RegisterForm({
   onShowPassword,
   showConfirmedPassword,
   onShowConfirmedPassword,
-  accesMode,
   onClickSignin,
 }) {
   const [input, setInput] = useState({
--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,6 @@ export default function Home() {
           onShowPassword={handleShowPassword}
           showConfirmedPassword={showConfirmedPassword}
           onShowConfirmedPassword={handleShowConfirmedPassword}
-          accessMode={accessMode}
           onClickSignin={handleClickSignin}
         />
       )}
