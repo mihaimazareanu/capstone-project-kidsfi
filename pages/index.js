991b7b04f418a89afcea5c49631014b2f40d1f19
Mihai Mazareanu <mihai.mazareanu@gmail.com>
2022-12-07 16:52:20 +0100

    another minor change

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,6 @@ export default function Home({
               onShowPassword={onShowPassword}
               signedIn={signedIn}
               onSignIn={onSignIn}
-              loginFilter={loginFilter}
               users={users}
             />
           )}
